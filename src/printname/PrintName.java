5335f5a802dbfcc130e3e0a540f0f7dc02dd993b
Taj Sidhu <tss_nn@yahoo.com>
2014-01-17 22:25:30 -0500

    Added that my name really is Tajinder Sidhu.

--- src/printname/PrintName.java
+++ src/printname/PrintName.java
@@ -5,6 +5,7 @@ public class PrintName {
     public void print()
     {
         System.out.println("My Name is Taj Sidhu");
+        System.out.println("I lied- my name is Tajinder Sidhu");
     }
     
 }
